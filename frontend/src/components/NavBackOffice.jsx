9db216e2c107e0efaea59b27d489b4294bfd786b
charliepian <charlie.piancatelli@gmail.com>
2023-01-11 17:43:19 +0100

    Ajout du css

--- frontend/src/components/NavBackOffice.jsx
+++ frontend/src/components/NavBackOffice.jsx
@@ -4,8 +4,18 @@ function NavBackOffice() {
   return (
     <nav className="p-7 w-full bg-black">
       <ul className="flex justify-center text-xl">
-        <Link to="/" className="hover:text-white focus:text-white text-white mr-4">Profile</Link>
-        <Link to="/Rocket" className="hover:text-white focus:text-white text-white ml-4">Rocket</Link>
+        <Link
+          to="/"
+          className="hover:text-white focus:text-white text-white mr-4"
+        >
+          Profile
+        </Link>
+        <Link
+          to="/Rocket"
+          className="hover:text-white focus:text-white text-white ml-4"
+        >
+          Rocket
+        </Link>
       </ul>
     </nav>
   );
