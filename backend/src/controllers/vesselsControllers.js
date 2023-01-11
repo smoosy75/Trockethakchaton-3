3d7db9bb70fbc86f10239e76e31cfd73985d1232
arouxel17 <alexandrerouxel17@gmail.com>
2023-01-11 20:06:31 +0100

    creation de toutes les routes + modif bdd

--- backend/src/controllers/vesselsControllers.js
+++ backend/src/controllers/vesselsControllers.js
@@ -1,7 +1,7 @@
 const models = require("../models");
 
 const browse = (req, res) => {
-  models.item
+  models.vessels
     .findAll()
     .then(([rows]) => {
       res.send(rows);
@@ -13,7 +13,7 @@ const browse = (req, res) => {
 };
 
 const read = (req, res) => {
-  models.item
+  models.vessels
     .find(req.params.id)
     .then(([rows]) => {
       if (rows[0] == null) {
@@ -29,14 +29,14 @@ const read = (req, res) => {
 };
 
 const edit = (req, res) => {
-  const item = req.body;
+  const vessels = req.body;
 
   // TODO validations (length, format...)
 
-  item.id = parseInt(req.params.id, 10);
+  vessels.id = parseInt(req.params.id, 10);
 
-  models.item
-    .update(item)
+  models.vessels
+    .update(vessels)
     .then(([result]) => {
       if (result.affectedRows === 0) {
         res.sendStatus(404);
@@ -51,14 +51,14 @@ const edit = (req, res) => {
 };
 
 const add = (req, res) => {
-  const item = req.body;
+  const vessels = req.body;
 
   // TODO validations (length, format...)
 
-  models.item
-    .insert(item)
+  models.vessels
+    .insert(vessels)
     .then(([result]) => {
-      res.location(`/items/${result.insertId}`).sendStatus(201);
+      res.location(`/vessels/${result.insertId}`).sendStatus(201);
     })
     .catch((err) => {
       console.error(err);
@@ -67,7 +67,7 @@ const add = (req, res) => {
 };
 
 const destroy = (req, res) => {
-  models.item
+  models.vessels
     .delete(req.params.id)
     .then(([result]) => {
       if (result.affectedRows === 0) {
