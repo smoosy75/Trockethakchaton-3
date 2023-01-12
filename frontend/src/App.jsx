8ec818cb7bd50afcde7081b5378f5a49410988a5
arouxel17 <alexandrerouxel17@gmail.com>
2023-01-13 00:57:21 +0100

    mise à jour routes + import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,30 +6,25 @@ import NavBackOffice from "./components/Back-Office-Composant/NavBackOffice";
 import Acceuil from "./components/Acceuil";
 import RegisterPage from "./components/RegisterPage";
 import LoginPage from "./components/LoginPage";
+import Profile from "./pages/Back-office/Profile";
+import Rocket from "./pages/Back-office/Rocket";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Acceuil />} />
-          <Route path="/Departures" element={<Departures />} />
-          <Route path="/Login" element={<LoginPage />} />
-          <Route path="/Checkout" element={<Checkout />} />
-          <Route path="/Register" element={<RegisterPage />} />
-        </Routes>
-        {/* <Departures />
-      <Router></Router>
-        <div className="App">
-          <NavBackOffice />
-          <Routes>
-            <Route path="profile/:id" element={<Profile />} />
-            <Route path="rocket" element={<Rocket />} />
-          </Routes>
-        </div>
-      </Router> */}
-      </div>
+      <Routes>
+        <Route path="/" element={<Acceuil />} />
+        <Route path="/Departures" element={<Departures />} />
+        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Checkout" element={<Checkout />} />
+        <Route path="/Register" element={<RegisterPage />} />
+      </Routes>
+      <NavBackOffice />
+      <Routes>
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/rocket" element={<Rocket />} />
+      </Routes>
     </Router>
   );
 }
